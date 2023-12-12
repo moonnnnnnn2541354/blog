f70555d2f8367e25ac34a1687c961284707b2438
tast <ioae7979@gmail.com>
2023-12-12 17:01:33 +0900

    feat: WebSecurityConfig.java 기능 구현

--- src/main/java/com/sparta/blog/global/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/blog/global/config/WebSecurityConfig.java
@@ -1,16 +1,80 @@
 package com.sparta.blog.global.config;
 
+import com.sparta.blog.global.jwt.JwtUtil;
+import com.sparta.blog.global.jwt.repository.JwtRepository;
+import com.sparta.blog.global.security.JwtAuthenticationFilter;
+import com.sparta.blog.global.security.JwtAuthorizationFilter;
+import com.sparta.blog.global.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final JwtRepository jwtRepository;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration configuration;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+        throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, jwtRepository);
+        filter.setAuthenticationManager(authenticationManager(configuration));
+        return filter;
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtUtil, jwtRepository, userDetailsService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf((csrf) -> csrf.disable());
+
+        httpSecurity.sessionManagement(
+            (sessionManagement) -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        httpSecurity.authorizeHttpRequests(
+            (authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/api/users/**").permitAll()
+                .anyRequest().authenticated());
+
+        httpSecurity
+            .addFilterBefore(jwtAuthenticationFilter(), JwtAuthenticationFilter.class);
+        httpSecurity
+            .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
     }
 }
