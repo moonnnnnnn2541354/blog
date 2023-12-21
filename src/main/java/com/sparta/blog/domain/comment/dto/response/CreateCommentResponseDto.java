6cad5717becd70a1b46f8c7b5a9e10f72c5c14a1
tast <ioae7979@gmail.com>
2023-12-21 20:11:24 +0900

    refactor: Comment_CreateResponseDto date 필드값 추가

--- src/main/java/com/sparta/blog/domain/comment/dto/response/CreateCommentResponseDto.java
+++ src/main/java/com/sparta/blog/domain/comment/dto/response/CreateCommentResponseDto.java
@@ -2,6 +2,7 @@ package com.sparta.blog.domain.comment.dto.response;
 
 import com.sparta.blog.domain.comment.entity.Comment;
 import com.sparta.blog.domain.user.entity.User;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
@@ -11,12 +12,14 @@ public class CreateCommentResponseDto {
     private String username;
     private String text;
     private Boolean thumbsUp;
+    private LocalDateTime date;
 
     public CreateCommentResponseDto(Comment comment, User user) {
         this.id = comment.getId();
         this.username = user.getUsername();
         this.text = comment.getText();
         this.thumbsUp = comment.getThumbsUp();
+        this.date = comment.getCreatedAt();
     }
 
 }
