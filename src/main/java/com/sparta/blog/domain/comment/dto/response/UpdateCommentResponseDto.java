a6d63245a460493ed294cc80ce84c6f209a7403f
tast <ioae7979@gmail.com>
2023-12-22 11:39:37 +0900

    refector: Comment_thumbsUp 관련부분 삭제

--- src/main/java/com/sparta/blog/domain/comment/dto/response/UpdateCommentResponseDto.java
+++ src/main/java/com/sparta/blog/domain/comment/dto/response/UpdateCommentResponseDto.java
@@ -11,14 +11,12 @@ public class UpdateCommentResponseDto {
     private Long id;
     private String username;
     private String text;
-    private Boolean thumbsUp;
     private LocalDateTime date;
 
     public UpdateCommentResponseDto(Comment comment, User user) {
         this.id = comment.getId();
         this.username = user.getUsername();
         this.text = comment.getText();
-        this.thumbsUp = comment.getThumbsUp();
         this.date = comment.getModifiedAt();
     }
 
